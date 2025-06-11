54e3cdc4a61352555e0e6493c24ea8e8d16b7faa
Sajid-developer <msasajid01.pro@gmail.com>
2025-06-12 00:41:31 +0530

    js file created.

---
+++ index.js
@@ -0,0 +1,11 @@
+console.log("App is running successfully...");
+
+// declared and initialized an object of studentInfo
+const studentInfo = {
+   id: 34527,
+   name: "Shivraj Kumar",
+   dob: "24-05-2002",
+   course: "B.Com",
+   address: "New Delhi, India"
+};
+
